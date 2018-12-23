d15a9542446efea590fd5b9e3df438a6a356d690
Karoline Ødegård <karoode@Karolines-MacBook-Air.local>
2018-12-23 18:42:56 +0100

    del 1 ferdig

--- day1.rkt
+++ day1.rkt
@@ -1,11 +1,11 @@
 #lang racket
 
-(define in (open-input-file "day1input.txt"))
+(define (sum-lines infile)
+    (define (read-next-line infile sum)
+        (let ((line (read-line infile 'any)))
+         (if (eof-object? line)
+          (displayln sum)
+          (read-next-line infile (+ sum (string->number line))))))
+    (read-next-line infile 0))
 
-(define (read-next-line infile sum)
-    (let ((line (read-line infile 'any)))
-     (if (eof-object? line)
-      (displayln sum)
-      (read-next-line infile (+ sum (string->number line))))))
-
-(read-next-line in 0)
+(call-with-input-file "day1input.txt" sum-lines)
